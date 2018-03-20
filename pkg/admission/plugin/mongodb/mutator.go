1f456dbbe692de7935574d9adb5dbffd47429cdf
the-redback <maruf@appscode.com>
2018-03-20 15:56:46 +0600

    Snapshot Validation Extended

--- pkg/admission/plugin/mongodb/mutator.go
+++ pkg/admission/plugin/mongodb/mutator.go
@@ -74,7 +74,6 @@ func (a *MongoDBMutator) Admit(req *admission.AdmissionRequest) *admission.Admis
 		if err != nil {
 			return hookapi.StatusBadRequest(err)
 		}
-		//mongodb := deepcopy
 		mongoMod, err := mgm.OnCreate(a.client, a.extClient.KubedbV1alpha1(), obj.(*api.MongoDB).DeepCopy())
 		if err != nil {
 			return hookapi.StatusForbidden(err)
