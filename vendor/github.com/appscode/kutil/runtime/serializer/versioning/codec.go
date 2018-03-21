ec4e2c3376770450528882ecd8a0f79168686880
the-redback <maruf@appscode.com>
2018-03-21 14:28:48 +0600

    Revendored [ci skip]

--- vendor/github.com/appscode/kutil/runtime/serializer/versioning/codec.go
+++ vendor/github.com/appscode/kutil/runtime/serializer/versioning/codec.go
@@ -15,7 +15,7 @@ import (
 	_ "k8s.io/kubernetes/pkg/apis/extensions/install"
 )
 
-var serializer = func() runtime.Codec {
+var JSONSerializer = func() runtime.Codec {
 	mediaType := "application/json"
 	info, ok := runtime.SerializerInfoForMediaType(legacyscheme.Codecs.SupportedMediaTypes(), mediaType)
 	if !ok {
@@ -24,20 +24,38 @@ var serializer = func() runtime.Codec {
 	return info.Serializer
 }()
 
+var YAMLSerializer = func() runtime.Codec {
+	mediaType := "application/yaml"
+	info, ok := runtime.SerializerInfoForMediaType(legacyscheme.Codecs.SupportedMediaTypes(), mediaType)
+	if !ok {
+		panic("unsupported media type " + mediaType)
+	}
+	return info.Serializer
+}()
+
 type codec struct {
+	encoder runtime.Encoder
+	decoder runtime.Decoder
 	scheme        *runtime.Scheme
+	defaulter     runtime.ObjectDefaulter
 	encodeVersion schema.GroupVersion
 	decodeVersion schema.GroupVersion
 }
 
 // NewDefaultingCodecForScheme is a convenience method for callers that are using a scheme.
 func NewDefaultingCodecForScheme(
+	encoder runtime.Encoder,
+	decoder runtime.Decoder,
 	scheme *runtime.Scheme,
+	defaulter runtime.ObjectDefaulter,
 	encodeVersion schema.GroupVersion,
 	decodeVersion schema.GroupVersion,
 ) runtime.Codec {
 	return codec{
+		encoder:   encoder,
+		decoder:   decoder,
 		scheme:        scheme,
+		defaulter:     defaulter,
 		encodeVersion: encodeVersion,
 		decodeVersion: decodeVersion,
 	}
@@ -58,21 +76,27 @@ func (c codec) Encode(obj runtime.Object, w io.Writer) error {
 			return err
 		}
 	}
-	c.scheme.Default(out)
+	if c.defaulter != nil {
+		c.defaulter.Default(out)
+	}
 
-	return serializer.Encode(out, w)
+	return c.encoder.Encode(out, w)
 }
 
 func (c codec) Decode(data []byte, gvk *schema.GroupVersionKind, _ runtime.Object) (runtime.Object, *schema.GroupVersionKind, error) {
-	in, gvk, err := serializer.Decode(data, gvk, nil)
+	in, gvk, err := c.decoder.Decode(data, gvk, nil)
 	if err != nil {
 		return nil, gvk, err
 	}
-	c.scheme.Default(in)
-
 	if gvk.GroupVersion() != c.encodeVersion {
 		return nil, gvk, fmt.Errorf("data expected to be of version %s, found %s", c.encodeVersion, gvk)
 	}
+
+	if c.defaulter != nil {
+		c.defaulter.Default(in)
+	}
+	in.GetObjectKind().SetGroupVersionKind(*gvk)
+
 	if c.encodeVersion == c.decodeVersion {
 		return in, gvk, err
 	}
